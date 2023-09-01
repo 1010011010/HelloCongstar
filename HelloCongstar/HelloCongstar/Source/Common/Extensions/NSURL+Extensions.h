b933936ce600c336e9db9e4514b946d4dd64a33d
Ilja Modrau <ilja.modrau.extern@congstar.net>
2023-09-01 15:05:57 +0200

    add bridging header

--- HelloCongstar/HelloCongstar/Source/Common/Extensions/NSURL+Extensions.h
+++ HelloCongstar/HelloCongstar/Source/Common/Extensions/NSURL+Extensions.h
@@ -1,7 +1,11 @@
 #import <Foundation/Foundation.h>
 
+NS_ASSUME_NONNULL_BEGIN
+
 @interface NSURL (QueryItem)
 
-- (NSURL *)URLByAppendingQueryItemWithName:(NSString *)name value:(NSString *)value;
+- (NSURL *)URLByAppendingQueryItemWithName:(NSString *)name value:(nullable NSString *)value;
 
 @end
+
+NS_ASSUME_NONNULL_END
--- HelloCongstar/HelloCongstarTests/URLExtensionTests.swift
+++ HelloCongstar/HelloCongstarTests/URLExtensionTests.swift
@@ -1,40 +1,41 @@
-//import Foundation
-//
-//@testable import HelloCongstar
-//import XCTest
-//
-//class URLExtensionsTests: XCTestCase {
-//    
-//    func testAppendQueryItem() {
-//        // Given
-//        let url = NSURL(string: "https://example.com")!
-//        
-//        // When
-//        let newURL = url.urlb
-//        
-//        // Then
-//        XCTAssertEqual(newURL.absoluteString, "https://example.com?key=value")
-//    }
-//    
-//    func testAppendQueryItemWhenExistingQuery() {
-//        // Given
-//        let url = URL(string: "https://example.com?existingKey=existingValue")!
-//        
-//        // When
-//        let newURL = url.appendingQueryItem(name: "key", value: "value")
-//        
-//        // Then
-//        XCTAssertEqual(newURL.absoluteString, "https://example.com?existingKey=existingValue&key=value")
-//    }
-//    
-//    func testAppendQueryItemWithNilValue() {
-//        // Given
-//        let url = URL(string: "https://example.com")!
-//        
-//        // When
-//        let newURL = url.appendingQueryItem(name: "key", value: nil)
-//        
-//        // Then
-//        XCTAssertEqual(newURL.absoluteString, "https://example.com?key")
-//    }
-//}
+import Foundation
+
+@testable import HelloCongstar
+import XCTest
+
+class URLExtensionsTests: XCTestCase {
+    
+    func testAppendQueryItem() {
+        // Given
+        let url = NSURL(string: "https://example.com")!
+        
+        // When
+        let newURL = url.appendingQueryItem(withName: "key", value: "value")
+        
+        // Then
+        XCTAssertEqual(newURL.absoluteString, "https://example.com?key=value")
+    }
+    
+    func testAppendQueryItemWhenExistingQuery() {
+        // Given
+        let url = NSURL(string: "https://example.com?existingKey=existingValue")!
+        
+        // When
+        let newURL = url.appendingQueryItem(withName: "key", value: "value")
+        
+        // Then
+        XCTAssertEqual(newURL.absoluteString, "https://example.com?existingKey=existingValue&key=value")
+    }
+    
+    func testAppendQueryItemWithNilValue() {
+        // Given
+        let url = NSURL(string: "https://example.com")!
+        
+        // When
+        let newURL = url.appendingQueryItem(withName: "key", value: nil)
+        
+        // Then
+        XCTAssertEqual(newURL.absoluteString, "https://example.com?key")
+    }
+}
+
