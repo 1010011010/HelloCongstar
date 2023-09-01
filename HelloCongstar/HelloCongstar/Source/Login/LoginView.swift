d930909f65946b9f3e6b179a13fd56454c05c25e
Ilja Modrau <ilja.modrau.extern@congstar.net>
2023-09-01 11:09:36 +0200

    Add login button

--- HelloCongstar/HelloCongstar/Source/Login/LoginView.swift
+++ HelloCongstar/HelloCongstar/Source/Login/LoginView.swift
@@ -1,8 +1,43 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @ObservedObject
+    var viewModel = LoginViewModel(loginRepository: LoginRepositoryMock())
+    
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                TextField(text: $viewModel.userName) {
+                    Text("User Name")
+                }
+                .background(Color.white)
+                .accentColor(.white)
+                
+                SecureField("Enter a password", text: $viewModel.password)
+                    .background(Color.white)
+                
+                Button("Login") {
+                    Task {
+                        await viewModel.login()
+                    }
+                }
+            }
+            
+            .padding()
+        }
+        .sheet(item: $viewModel.user) { user in
+            VStack {
+                Text(user.name)
+                Text(user.lastName)
+            }
+        }
     }
 }
 
--- HelloCongstar/HelloCongstar/Source/Login/LoginViewModel.swift
+++ HelloCongstar/HelloCongstar/Source/Login/LoginViewModel.swift
@@ -1,21 +1,32 @@
 import Foundation
 import SwiftUI
 
-@MainActor
 final class LoginViewModel: ObservableObject {
 
+    struct Item: Identifiable {
+        let id = UUID()        
+        let name: String
+        let lastName: String
+    }
+    
+    
     @Published var userName: String = ""
     @Published var password: String = ""
-    @Published var user: UserModel?
-
+    
+    @Published var user: Item?
+    
+    
     private let loginRepository: LoginRepository
 
     init(loginRepository: LoginRepository) {
         self.loginRepository = loginRepository
     }
 
+    @MainActor
     func login() async {
         let loginModel = LoginModel(userName: userName, password: password)
-        user = await loginRepository.doLogin(login: loginModel)
+        let user = await loginRepository.doLogin(login: loginModel)
+        let item = Item(name: user.name, lastName: user.lastName)
+        self.user = item
     }
 }
