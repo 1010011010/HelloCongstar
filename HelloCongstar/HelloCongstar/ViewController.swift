3656f7a5092e57dbcf4909d0caa4e3142f5fd2d6
Ilja Modrau <ilja.modrau.extern@congstar.net>
2023-09-01 08:02:05 +0200

    Add Xcode project

---
+++ HelloCongstar/HelloCongstar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloCongstar
+//
+//  Created by Ilja Modrau on 01.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
